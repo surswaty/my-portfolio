ef6e8aeee3628df2e81b76a053d170e178ca0c35
Reman Swati <yemshams@gmail.com>
2023-05-27 11:05:21 +0500

    created the nav bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import Intro from "./Intro";
+import Nav from "./Nav";
+import Contact from "./Contact"
 
 function App() {
   return (
     <div>
-      
+      <Nav />
       <Intro />
+      <Contact />
     </div>
   );
 }
---
+++ src/Nav.js
@@ -0,0 +1,36 @@
+
+const Nav = () => {
+    return(
+        <nav className="font-mono max-w-7xl p-16">
+            <ul className="flex flex-wrap md:justify-end justify-center  space-x-8 space-y-8 m-0 p-0 list-none">
+                <li className="lg:w-full text-2xl font-bold w-full ">
+                    <h1>
+                        <a href="index.html" className="font-sans">
+                            <span className="fas fa-code text-aqua hover:text-hotmag mr-2"></span>
+                            <span className="text-white hover:text-hotmag">Rahman Swati.</span>
+                        </a>
+                    </h1>
+                </li>
+                <li className=""><a className="text-white" href="#projects">Projects</a></li>
+                <li><a className="text-white" href="about.html">About</a></li>
+                <li><a className="text-white" href="#contacts">Contacts</a></li>
+                <li>
+                    <a href="https://linkedin.com/in/remanswati" >
+                        <span className="fab fa-linkedin text-aqua hover:text-hotmag text-2xl"></span>
+                        <span className="sr-only">linkedIn</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://github.com/remanswaty/">
+                        <span className="fab fa-github text-aqua hover:text-hotmag text-2xl "></span>
+                        <span className="sr-only">Github</span>
+                    </a>
+                </li>
+                <li className=""><a href="" target="blank" className="button bg-magenta text-white p-2 rounded hover:text-white hover:bg-hotmag ">Resume</a></li>
+                
+            </ul>
+        </nav>
+    );
+}
+
+export default Nav;
