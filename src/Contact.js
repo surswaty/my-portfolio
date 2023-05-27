7056c350fb7f660e0445e3369daca59c981fe89e
Reman Swati <yemshams@gmail.com>
2023-05-27 12:01:47 +0500

    contact section completed

---
+++ src/Contact.js
@@ -0,0 +1,14 @@
+
+const Contact = () => {
+    return (
+        <div id="contact" className="bg-plum">
+            <div className="lg:p-16 px-4 py-16 m-0 m-auto w-[300px] md:w-[500px] text-center">
+                <h2 className="text-3xl font-bold pb-6">Contact Me</h2>
+                <p className="text-xl pb-10">I'm always interested in bringing new ideas into reality, Making smart contracts, or creating a complete Web3 Dapp.</p>
+                <a href="mailto:yemshams@gmail.com" className="bg-magenta text-white p-2 rounded hover:text-white hover:bg-hotmag">Email Me</a>
+            </div>
+        </div>
+    );
+}
+
+export default Contact;
